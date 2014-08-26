c9371e19f3881a4b2e24393fc5ef841ddfcc9f4d
Pier Fumagalli <ianosh@gmail.com>
2014-08-26 21:16:29 +0900

    Start checking HTTPs

--- source/test/org/usrz/libs/httpd/ServerBuilderTest.java
+++ source/test/org/usrz/libs/httpd/ServerBuilderTest.java
@@ -17,11 +17,23 @@ package org.usrz.libs.httpd;
 
 import java.io.File;
 import java.net.URL;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.testng.annotations.Test;
 import org.usrz.libs.configurations.Configurations;
@@ -40,6 +52,7 @@ public class ServerBuilderTest extends AbstractTest {
     throws Exception {
         final File accessLog = IO.makeTempFile();
         final File documentRoot = IO.makeTempDir();
+        final File keystoreFile = IO.copyTempFile("selfsigned.pem");
         IO.copy("index.html", new File(documentRoot, "index.html"));
 
         final int port1 = NET.serverPort();
@@ -48,15 +61,21 @@ public class ServerBuilderTest extends AbstractTest {
 
         final Configurations configurations = new ConfigurationsBuilder()
             .put("server.name", "myServer123")
+
             .put("server.listener.host", "127.0.0.1")
             .put("server.listener.port", port1)
             .put("server.listener.secure", false)
+
             .put("server.listeners.0.host", "127.0.0.1")
             .put("server.listeners.0.port", port2)
             .put("server.listeners.0.secure", false)
+
             .put("server.listeners.1.host", "127.0.0.1")
             .put("server.listeners.1.port", port3)
-            .put("server.listeners.1.secure", false)
+            .put("server.listeners.1.secure", true)
+            .put("server.listeners.1.keystore_file", keystoreFile)
+            .put("server.listeners.1.keystore_password", "asdf")
+
             .put("server.access_log.file", accessLog)
             .put("server.access_log.synchronous", true) // for tests, easy
             .put("server.access_log.format", "%A:%p %r") // only GET /foo HTTP/1.1
@@ -111,6 +130,61 @@ public class ServerBuilderTest extends AbstractTest {
                                               .toInstance(dependency));
         });
 
+        /* Hairy code, trust ourselves for SSL certificate validation */
+        final KeyStore keyStore = KeyStore.getInstance("PEM");
+        keyStore.load(IO.resource("selfsigned.pem"), "asdf".toCharArray());
+        final Certificate certificate = keyStore.getCertificate("F7A4FD46266A272B145B4F09F6D14CC7A458268B");
+        assertNotNull(certificate, "Unable to find our own certificate???");
+
+        /* Trust manager trusting only ourselves... */
+        final SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, new TrustManager[] {
+                new X509TrustManager() {
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[]{};
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] certs, String authType)
+                    throws CertificateException {
+                        throw new CertificateException("NO");
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] certs, String authType)
+                    throws CertificateException {
+                        if (certificate.equals(certs[0])) return;
+                        throw new CertificateException("Invalid server cert: " + certs[0].getSubjectDN());
+                    }
+                }
+            }, new SecureRandom());
+
+        /* Install the security context with our trust manager */
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+        /* Verify host names */
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                try {
+                    if (certificate.equals(session.getPeerCertificates()[0])) {
+                        if ("127.0.0.1".equals(hostname)) return true;
+
+                        log.error("Wrong hostname: expected \"127.0.0.1\", got \"%s\"", hostname);
+                        return false;
+                    } else {
+                        log.error("Wrong certificate: got \"%s\"", session.getPeerPrincipal());
+                        return false;
+                    }
+                } catch (Exception exception) {
+                    log.error("Exception validating host", exception);
+                    return false;
+                }
+            }
+        });
+
+        /* Connect to the client and run tests */
         try {
             /* Check our server name */
             assertEquals(starter.server().getServerConfiguration().getName(), "myServer123");
@@ -121,10 +195,10 @@ public class ServerBuilderTest extends AbstractTest {
             for (int x = 0; x < 100; x ++) {
                 byte[] index1 = IO.read(new URL("http://127.0.0.1:" + port1 + "/index.html"));
                 byte[] index2 = IO.read(new URL("http://127.0.0.1:" + port2 + "/index.html"));
-                byte[] index3 = IO.read(new URL("http://127.0.0.1:" + port3 + "/index.html"));
+                byte[] index3 = IO.read(new URL("https://127.0.0.1:" + port3 + "/index.html"));
                 assertEquals(index1, index, "Wrong data received from port " + port1);
                 assertEquals(index2, index, "Wrong data received from port " + port2);
-                assertEquals(index3, index, "Wrong data received from port " + port3);
+                assertEquals(index3, index, "Wrong data received from SSL port " + port3);
             }
 
             /* Reaad our REST values and check them */
@@ -132,7 +206,7 @@ public class ServerBuilderTest extends AbstractTest {
             for (int x = 0; x < 100; x ++) {
                 final String string1 = new String(IO.read(new URL("http://127.0.0.1:" + port1 + "/rest1/")));
                 final String string2 = new String(IO.read(new URL("http://127.0.0.1:" + port2 + "/rest2/")));
-                final String string3 = new String(IO.read(new URL("http://127.0.0.1:" + port3 + "/rest3/")));
+                final String string3 = new String(IO.read(new URL("https://127.0.0.1:" + port3 + "/rest3/")));
                 assertEquals(string1, "{\"config\":\"config1\",\"depended_map\":{\"bar\":321,\"foo\":123},\"epoch_date\":0}");
                 assertEquals(string2, "{\"config\":\"config2\",\"dependedMap\":{\"bar\":321,\"foo\":123},\"epochDate\":\"1970-01-01T00:00:00.000+0000\"}");
                 assertEquals(string3, "{\n  \"Config\" : \"config3\",\n  \"DependedMap\" : {\n    \"bar\" : 321,\n    \"foo\" : 123\n  },\n  \"EpochDate\" : \"1970-01-01T00:00:00.000+0000\"\n}");
